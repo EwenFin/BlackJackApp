778e2d40db27ee17a14e50d5d2bb4e76cb94f847
Ewen Carr <ewencarr@hotmail.com>
2017-03-18 11:22:48 +0000

    added card images, broke everything again

--- app/src/main/java/com/codeclan/blackjack/BlackJackActivity.java
+++ app/src/main/java/com/codeclan/blackjack/BlackJackActivity.java
@@ -1,9 +1,12 @@
 package com.codeclan.blackjack;
 
+import android.content.Context;
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -13,6 +16,9 @@ public class BlackJackActivity extends AppCompatActivity {
     TextView gameText;
     TextView yourhandtext;
     TextView yournewhandtext;
+    ImageView image;
+    ImageView yourhand2;
+    ImageView yournewhand;
     BlackJack blackJack;
     Player player1;
     Player dealer;
@@ -44,15 +50,29 @@ public class BlackJackActivity extends AppCompatActivity {
         gameText.setText("Welcome to the BlackJack Table " + name);
     }
 
+    public static int getDrawable(Context context, String cardname){
+        return context.getResources().getIdentifier(cardname, "drawable", "com.codeclan.blackjack");
+
+    }
+
     public void onDealButtonClicked(View view) {
 
         player1hand.discardHand();
         deck.shuffle();
         blackJack.deal();
-        String yourhand = player1hand.showHand();
-        yourhandtext = (TextView)findViewById(R.id.your_hand_text);
-        Card dealercard = (Card) dealerhand.getHand().get(0);
-        yourhandtext.setText("You have " + yourhand + " for a score of " + player1hand.handValue() + "\n \n The Dealer is showing the " + dealercard.showCard());
+        String card1name = (player1hand.getHand().get(0)).toString();
+        String card2name = (player1hand.getHand().get(1)).toString();
+        int card1id = getDrawable(this, card1name);
+        int card2id = getDrawable(this, card2name);
+        image.setImageResource(card1id);
+        image.setImageResource(card2id);
+
+
+
+//      String yourhand = player1hand.showHand();
+//      yourhandtext = (TextView)findViewById(R.id.your_hand_text);
+//      Card dealercard = (Card) dealerhand.getHand().get(0);
+//      yourhandtext.setText("You have " + yourhand + " for a score of " + player1hand.handValue() + "\n \n The Dealer is showing the " + dealercard.showCard());
 
     }
 
