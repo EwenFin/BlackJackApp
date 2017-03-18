7b08e067fa53f60a4556db2f8b9d86aa3202e020
Ewen Carr <ewencarr@hotmail.com>
2017-03-18 12:22:57 +0000

    Added array for cardIds

--- app/src/main/java/com/codeclan/blackjack/BlackJackActivity.java
+++ app/src/main/java/com/codeclan/blackjack/BlackJackActivity.java
@@ -16,7 +16,8 @@ public class BlackJackActivity extends AppCompatActivity {
     TextView gameText;
     TextView yourhandtext;
     TextView yournewhandtext;
-    ImageView image;
+    ImageView card1image;
+    ImageView card2image;
     ImageView yourhand2;
     ImageView yournewhand;
     BlackJack blackJack;
@@ -26,6 +27,61 @@ public class BlackJackActivity extends AppCompatActivity {
     Deck deck;
     Hand player1hand;
     Hand dealerhand;
+
+    private final static int[] cardIds = new int[]{
+            R.drawable.ace_of_spades,
+            R.drawable.two_of_spades,
+            R.drawable.three_of_spades,
+            R.drawable.four_of_spades,
+            R.drawable.five_of_spades,
+            R.drawable.six_of_spades,
+            R.drawable.seven_of_spades,
+            R.drawable.eight_of_spades,
+            R.drawable.nine_of_spades,
+            R.drawable.ten_of_spades,
+            R.drawable.jack_of_spades,
+            R.drawable.queen_of_spades,
+            R.drawable.king_of_spades,
+            R.drawable.ace_of_clubs,
+            R.drawable.two_of_clubs,
+            R.drawable.three_of_clubs,
+            R.drawable.four_of_clubs,
+            R.drawable.five_of_clubs,
+            R.drawable.six_of_clubs,
+            R.drawable.seven_of_clubs,
+            R.drawable.eight_of_clubs,
+            R.drawable.nine_of_clubs,
+            R.drawable.ten_of_clubs,
+            R.drawable.jack_of_clubs,
+            R.drawable.queen_of_clubs,
+            R.drawable.king_of_clubs,
+            R.drawable.ace_of_diamonds,
+            R.drawable.two_of_diamonds,
+            R.drawable.three_of_diamonds,
+            R.drawable.four_of_diamonds,
+            R.drawable.five_of_diamonds,
+            R.drawable.six_of_diamonds,
+            R.drawable.seven_of_diamonds,
+            R.drawable.eight_of_diamonds,
+            R.drawable.nine_of_diamonds,
+            R.drawable.ten_of_diamonds,
+            R.drawable.jack_of_diamonds,
+            R.drawable.queen_of_diamonds,
+            R.drawable.king_of_diamonds,
+            R.drawable.ace_of_hearts,
+            R.drawable.two_of_hearts,
+            R.drawable.three_of_hearts,
+            R.drawable.four_of_hearts,
+            R.drawable.five_of_hearts,
+            R.drawable.six_of_hearts,
+            R.drawable.seven_of_hearts,
+            R.drawable.eight_of_hearts,
+            R.drawable.nine_of_hearts,
+            R.drawable.ten_of_hearts,
+            R.drawable.jack_of_hearts,
+            R.drawable.queen_of_hearts,
+            R.drawable.king_of_hearts,
+        };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,8 +106,8 @@ public class BlackJackActivity extends AppCompatActivity {
         gameText.setText("Welcome to the BlackJack Table " + name);
     }
 
-    public static int getDrawable(Context context, String cardname){
-        return context.getResources().getIdentifier(cardname, "drawable", "com.codeclan.blackjack");
+//    public static int getDrawable(Context context, String cardname){
+//        return context.getResources().getIdentifier(cardname, "drawable", "com.codeclan.blackjack");
 
     }
 
@@ -60,12 +116,12 @@ public class BlackJackActivity extends AppCompatActivity {
         player1hand.discardHand();
         deck.shuffle();
         blackJack.deal();
-        String card1name = (player1hand.getHand().get(0)).toString();
-        String card2name = (player1hand.getHand().get(1)).toString();
-        int card1id = getDrawable(this, card1name);
-        int card2id = getDrawable(this, card2name);
-        image.setImageResource(card1id);
-        image.setImageResource(card2id);
+
+        card1image = (ImageView)findViewById(R.id.card1image);
+        card1image.setImageResource(card1id);
+        card2image = (ImageView)findViewById(R.id.card2image);
+        card2image.setImageResource(card2id);
+
 
 
 
@@ -79,16 +135,20 @@ public class BlackJackActivity extends AppCompatActivity {
     public void onHitButtonClicked(View view){
         deck.shuffle();
         blackJack.hit(player1);
-        String yourhand = player1hand.showHand();
-        if(player1hand.handValue() > 21){
-            yournewhandtext = (TextView)findViewById(R.id.your_new_hand_text);
-            yournewhandtext.setText("You have " + yourhand + " for a score of " + player1hand.handValue() + " You've busted out!");
+//        String yourhand = player1hand.showHand();
+//        if(player1hand.handValue() > 21){
+//            yournewhandtext = (TextView)findViewById(R.id.your_new_hand_text);
+//            yournewhandtext.setText("You have " + yourhand + " for a score of " + player1hand.handValue() + " You've busted out!");
         }
+
+    public void onStayButtonClicked(View view){
+
+    }
 
 
     }
 
-}
+
 
 
 
