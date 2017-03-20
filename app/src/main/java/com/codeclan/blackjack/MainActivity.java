ebf957528e494266652c505683b45a7733dd7db1
Ewen Carr <ewencarr@hotmail.com>
2017-03-20 16:04:07 +0000

    welcome image

--- app/src/main/java/com/codeclan/blackjack/MainActivity.java
+++ app/src/main/java/com/codeclan/blackjack/MainActivity.java
@@ -1,11 +1,13 @@
 package com.codeclan.blackjack;
 
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     TextView welcome;
+    ImageView welcomeImage;
     EditText NameEntry;
     Button BlackJackButton;
 
@@ -29,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
 
         welcome = (TextView)findViewById(R.id.welcome);
         welcome.setText("Welcome to CodeClanCasino, please gamble responsibly...");
+
+        int welcomeImageId = this.getResources().getIdentifier("casino", "drawable", "com.codeclan.blackjack");
+        welcomeImage = (ImageView)findViewById(R.id.welcome_image);
+        welcomeImage.setImageResource(welcomeImageId);
+
     }
 
 
