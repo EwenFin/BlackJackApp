57339c448b3b54cd0ad5c891ddbd8766e5292769
Ewen Carr <ewencarr@hotmail.com>
2017-03-22 14:47:36 +0000

    Some pokerhand tests working

--- app/src/main/java/com/codeclan/blackjack/PokerHandChecker.java
+++ app/src/main/java/com/codeclan/blackjack/PokerHandChecker.java
@@ -3,6 +3,7 @@ package com.codeclan.blackjack;
 import java.util.ArrayList;
 import java.util.Collections;
 
+
 public class PokerHandChecker{
 
     private Hand hand;
@@ -24,7 +25,69 @@ public PokerHandChecker(Hand hand, ArrayList<Card> board){
             value = value + 14;
         }
         return value;
+
     }
 
-    public
+    public Enum multipleKinds(){
+        ArrayList<Card> allCards = new ArrayList<Card>();
+        ArrayList<Enum> ranks = new ArrayList<Enum>();
+        allCards.addAll(hand.getHand());
+        allCards.addAll(board);
+        for(Card card : allCards) {
+            ranks.add(card.getRank());
+        }
+        for(Enum rank : ranks){
+        if(Collections.frequency(ranks, rank) == 4) {
+            return PokerHandRank.FOUR_OF_A_KIND;
+             }
+        if(Collections.frequency(ranks, rank) == 3) {
+            return PokerHandRank.THREE_OF_A_KIND;
+            }
+         if(Collections.frequency(ranks, rank) == 2){
+             return PokerHandRank.PAIR;
+         }
+
+        }
+        return null;
+
+
+    }
+
+    public Enum isFlush(){
+        ArrayList<Card> allCards = new ArrayList<Card>();
+        ArrayList<Enum> suits = new ArrayList<Enum>();
+        allCards.addAll(hand.getHand());
+        allCards.addAll(board);
+        for(Card card : allCards) {
+            suits.add(card.getSuit());
+        }
+        for(Enum suit : suits)
+        if(Collections.frequency(suits, suit) >= 5){
+            return PokerHandRank.FLUSH;
+        }
+        return null;
+
+    }
+
+    public Enum isStraight(){
+        ArrayList<Card> allCards = new ArrayList<Card>();
+        allCards.addAll(hand.getHand());
+        allCards.addAll(board);
+        Collections.sort(allCards);
+        int position = 0;
+        int cardsTogether = 0;
+        for(int i = 0; i < 7; i ++) {
+            if (allCards.get(position + 1).getRank().ordinal() - allCards.get(position).getRank().ordinal() == 1) {
+                cardsTogether++;
+            }
+            position ++;
+            if(cardsTogether >= 4){
+                return PokerHandRank.STRAIGHT;
+
+            }
+        }
+        return null;
+
+    }
+
 }
--- app/src/test/java/com/codeclan/blackjack/PokerHandCheckerTest.java
+++ app/src/test/java/com/codeclan/blackjack/PokerHandCheckerTest.java
@@ -31,7 +31,8 @@ public class PokerHandCheckerTest {
 
     Deck deck;
 
-    PokerHandChecker checker;
+    PokerHandChecker checker1;
+    PokerHandChecker checker2;
 
     @Before
     public void before() {
@@ -54,6 +55,8 @@ public class PokerHandCheckerTest {
         card7 = new Card(Suit.HEARTS, Rank.FOUR);
         card8 = new Card(Suit.HEARTS, Rank.JACK);
         card9 = new Card(Suit.SPADES, Rank.JACK);
+        checker1 = new PokerHandChecker(player1hand,board);
+        checker2 = new PokerHandChecker(player2hand,board);
 
 
     }
@@ -64,13 +67,78 @@ public class PokerHandCheckerTest {
         player1hand.addCardToHand(card9);
         player2hand.addCardToHand(card2);
         player2hand.addCardToHand(card7);
-        checker = new PokerHandChecker(player1hand,board);
-        assertEquals(14, checker.highCard());
-        checker = new PokerHandChecker(player2hand,board);
-        assertEquals(10, checker.highCard());
+        assertEquals(14, checker1.highCard());
+        assertEquals(10, checker2.highCard());
     }
 
-    
+
+    @Test
+    public void testPair(){
+        player1hand.addCardToHand(card9);
+        player1hand.addCardToHand(card1);
+        player2hand.addCardToHand(card8);
+        player2hand.addCardToHand(card7);
+        board.add(card2);
+        board.add(card3);
+        board.add(card4);
+        board.add(card5);
+        board.add(card6);
+        assertEquals(PokerHandRank.PAIR, checker1.multipleKinds());
+
+    }
+
+    @Test
+    public void testFourOfAKind(){
+        card3 = new Card (Suit.CLUBS, Rank.JACK);
+        player1hand.addCardToHand(card8);
+        player1hand.addCardToHand(card9);
+        board.add(card3);
+        board.add(card2);
+        board.add(card4);
+        board.add(card5);
+        board.add(card6);
+        assertEquals(PokerHandRank.FOUR_OF_A_KIND, checker1.multipleKinds());
+    }
+
+    @Test
+    public void testThreeOfAKind(){
+        player1hand.addCardToHand(card8);
+        player1hand.addCardToHand(card9);
+        board.add(card3);
+        board.add(card2);
+        board.add(card4);
+        board.add(card5);
+        board.add(card6);
+        assertEquals(PokerHandRank.THREE_OF_A_KIND, checker1.multipleKinds());
+    }
+
+    @Test
+    public void testIsFlush(){
+        player1hand.addCardToHand(card1);
+        player1hand.addCardToHand(card5);
+        board.add(card6);
+        board.add(card7);
+        board.add(card8);
+        board.add(card9);
+        board.add(card2);
+        assertEquals(PokerHandRank.FLUSH, checker1.isFlush());
+
+    }
+
+    @Test
+    public void testisStraight(){
+        player1hand.addCardToHand(card1);
+        player1hand.addCardToHand(card2);
+        Card card3 = new Card(Suit.HEARTS, Rank.NINE);
+        Card card4 = new Card(Suit.SPADES, Rank.QUEEN);
+        board.add(card3);
+        board.add(card4);
+        board.add(card5);
+        board.add(card9);
+        board.add(card7);
+        assertEquals(PokerHandRank.STRAIGHT, checker1.isStraight());
+    }
+
 
 
 }
