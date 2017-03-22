24d8c77c358c192b504b71dd912e3ae8e7ab24c4
Ewen Carr <ewencarr@hotmail.com>
2017-03-22 09:23:41 +0000

    Poker hand rank enums and poker test added

---
+++ app/src/main/java/com/codeclan/blackjack/PokerHandRank.java
@@ -0,0 +1,17 @@
+package com.codeclan.blackjack;
+
+
+public enum PokerHandRank {
+
+    HIGH_CARD,
+    PAIR,
+    TWO_PAIR,
+    THREE_OF_A_KIND,
+    STRAIGHT,
+    FLUSH,
+    FULL_HOUSE,
+    FOUR_OF_A_KIND,
+    STRAIGHT_FLUSH,
+
+
+}
--- app/src/test/java/com/codeclan/blackjack/PokerHandCheckerTest.java
+++ app/src/test/java/com/codeclan/blackjack/PokerHandCheckerTest.java
@@ -1,8 +1,48 @@
 package com.codeclan.blackjack;
 
-/**
- * Created by user on 21/03/2017.
- */
+
+import org.junit.Before;
+
+import java.util.ArrayList;
 
 public class PokerHandCheckerTest {
-}
+
+    Poker poker;
+
+    Player player1;
+    Player player2;
+    ArrayList<Player> players;
+    Hand player1hand;
+    Hand player2hand;
+
+    Card card1;
+    Card card2;
+    Card card3;
+    Card card4;
+    Card card5;
+    Card card6;
+    Card card7;
+    Card card8;
+    Card card9;
+
+    Deck deck;
+
+    @Before
+    public void before(){
+        deck = new Deck();
+        players = new ArrayList<Player>();
+        player1hand = new Hand();
+        player2hand = new Hand();
+        player1 = new Player("Ewen", player1hand);
+        player2 = new Player("Steven", player2hand);
+        players.add(player1);
+        players.add(player2);
+        poker = new Poker( players, deck);
+        card1 = new Card(Suit.HEARTS, Rank.KING);
+        card2 = new Card(Suit.DIAMONDS, Rank.JACK);
+        card3 = new Card(Suit.SPADES, Rank.ACE);
+        card4 = new Card(Suit.CLUBS, Rank.FOUR);
+        card5 = new Card(Suit.HEARTS, Rank.TEN);
+
+
+    }
